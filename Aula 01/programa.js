3f10060a6c2cd8207381750064e2d2c5f1572603
Daniel Savoy <dansavoy@gmail.com>
2020-08-20 20:57:16 -0300

    Aula01

--- Aula 01/programa.js
+++ Aula 01/programa.js
@@ -1,5 +1,19 @@
-window.addEventListener("load", main);
+window.addEventListener("mousemove", main);
 
-function main(){
-    document.body.style.background = "red";
+function main(evt){
+    let x = evt.x;
+    let y = evt.y;
+
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+
+    let hue = x/w * 360; // 360deg
+    let lux = y/h * 100; // 100%
+    let sat = 100; //100%
+
+    colorir(`hsl(${hue}deg,${sat}%,${lux}%)`);
+}
+
+function colorir(cor){
+    document.body.style.background = cor;
 }
