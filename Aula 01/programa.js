6c522815c4e59ef7253d8228447ed102b9774f2f
Daniel Savoy <dansavoy@gmail.com>
2020-08-20 21:42:19 -0300

    Aula01

--- Aula 01/programa.js
+++ Aula 01/programa.js
@@ -6,12 +6,29 @@ function main(evt){
 
     let w = window.innerWidth;
     let h = window.innerHeight;
-
+    /*
     let hue = x/w * 360; // 360deg
-    let lux = y/h * 100; // 100%
-    let sat = 100; //100%
+    let lux = 100 - y/h * 100; // 100%
+    let sat = 100; //100%*/
 
+    let t = x/w;
+    let cor = lerpCor([240,100,80], [60,100,50], t);
+    let hue = cor[0];
+    let sat = cor[1];
+    let lux = cor[2];
     colorir(`hsl(${hue}deg,${sat}%,${lux}%)`);
+}
+
+function lerpCor(c1,c2,t){
+    return [
+        lerp(c1[0],c2[0],t),
+        lerp(c1[1],c2[1],t),
+        lerp(c1[2],c2[2],t),
+    ];
+}
+
+function lerp(a, b, t){
+    return a*(1-t) + b*t; 
 }
 
 function colorir(cor){
