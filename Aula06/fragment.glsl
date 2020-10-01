ca2602be86579cae8d63d5131990430e16aee08c
Daniel Savoy <dansavoy@gmail.com>
2020-10-01 20:14:32 -0300

    aula 06

---
+++ Aula06/fragment.glsl
@@ -0,0 +1,15 @@
+precision highp float;
+
+uniform float frame;
+
+varying vec4 ponto;
+
+void main(){
+
+    // -1 <= X <= 1
+    float r = (ponto.x + 1.0) / 2.0;
+    float g = (ponto.y + 1.0) / 2.0;
+
+    // RGBA
+    gl_FragColor = vec4(1.0-g, 1.0-g, 1.0-g, 1.0);
+}
