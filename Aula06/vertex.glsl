2ea8973672bb34b4771898c8ed05258f31d35aa7
Daniel Savoy <dansavoy@gmail.com>
2020-10-01 21:46:32 -0300

    aula 06

--- Aula06/vertex.glsl
+++ Aula06/vertex.glsl
@@ -14,9 +14,9 @@ varying vec4 norm;
 
 void main(){
 
-    ponto = projection * view * model * vec4(position, 1.0);
+    ponto =  model * vec4(position, 1.0);
 
-    norm = view * vec4(normal.xyz, 0.0);
+    norm = vec4(normal.xyz, 0.0);
 
-    gl_Position = ponto;
+    gl_Position = projection * view * ponto;
 }
